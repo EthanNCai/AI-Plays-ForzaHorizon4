b82f34e875e989790f8e783ec70cfca5bd2d7a53
蔡俊志 <chickenbilibili@outlook.com>
2023-10-02 17:32:00 +0800

    optimized the code of data balancing

--- CNN/sortData.py
+++ CNN/sortData.py
@@ -1,62 +1,35 @@
 import numpy as np
 import pandas as pd
 from collections import Counter
-from random import shuffle
-import cv2
+import statistics
+import random
 
+# load data
+unbalanced_data = list(np.load('./train_data.npy', allow_pickle=True))
 
-train_data = list(np.load('../train_data.npy', allow_pickle=True))
-df = pd.DataFrame(train_data)
-counter = Counter(map(tuple, df[1]))
-balanced_count = min(counter[(1, 0, 0, 1)], counter[(1, 1, 0, 0)], counter[(1, 0, 0, 0)])*5
+# counting label & decide balancing target
+counter = Counter(map(tuple, pd.DataFrame(unbalanced_data)[1]))
+counter_list = [(label, count) for label, count in counter.items()]
+balancing_target = int(statistics.mean(count for _, count in counter_list))
 
+print('original_data:')
+print(counter)
 
-# W:100000
-# WA:2000
-# WD:3000
-
-# balanced_count = 10000
-
-
-# W:10000
-# WA:2000
-# WD:3000
-
-
-forward = []
-left = []
-right = []
-other = []
-
-for data in train_data:
-    img = data[0]
-    choice = data[1]
-    if (choice == [1, 0, 0, 0]).all():
-        forward.append([img, choice])
-    elif (choice == [1, 1, 0, 0]).all():
-        left.append([img, choice])
-    elif (choice == [1, 0, 0, 1]).all():
-        right.append([img, choice])
+# manipulate data according to balancing target
+balanced_data = []
+for label, count in counter_list:
+    label_data = [[i, l] for i, l in unbalanced_data if (label == l).all()]
+    if count < balancing_target:
+        balanced_data += label_data
     else:
-        other.append([img, choice])
+        balanced_data += random.sample(label_data, balancing_target)
 
-if balanced_count < len(forward):
-    forward = forward[:balanced_count]
-if balanced_count < len(left):
-    forward = forward[:balanced_count]
-if balanced_count < len(right):
-    forward = forward[:balanced_count]
-if balanced_count < len(other):
-    forward = forward[:balanced_count]
+# shuffle data
+random.shuffle(balanced_data)
+print('balanced_data')
+print(Counter(pd.DataFrame(balanced_data)[1].apply(str)))
 
-final_list = forward + left + right + other
-
-shuffle(final_list)
-f_df = pd.DataFrame(final_list)
-print(Counter(f_df[1].apply(str)))
-np.save('../balanced_train_data.npy', np.array(final_list, dtype=object))
-
-
+np.save('balanced_data', np.array(balanced_data, dtype=object))
 
 
 
